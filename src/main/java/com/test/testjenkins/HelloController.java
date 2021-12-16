704f896eb6abd2625780a6b422e7da65a09ca5d5
SeongeunYang <yse09@naver.com>
2021-12-16 18:20:40 +0900

    테스트 3

--- src/main/java/com/test/testjenkins/HelloController.java
+++ src/main/java/com/test/testjenkins/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String sayHello() {
-        return "Hello, Jenkins!! 자동 배포 테스트2222";
+        return "자동 배포 하고싶다..";
     }
 }
