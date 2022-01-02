28d3df6dc3088569beb0ca695b522c2f825f7d3c
SeongeunYang <yse09@naver.com>
2022-01-02 19:29:45 +0900

    배포자동화 travis

--- src/main/java/com/test/testjenkins/HelloController.java
+++ src/main/java/com/test/testjenkins/HelloController.java
@@ -1,12 +1,35 @@
 package com.test.testjenkins;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
-    @GetMapping
+    private final Environment env;
+
+
+    @GetMapping("/")
+    public String checkPort() {
+        List<String> portCheck = Arrays.asList(env.getActiveProfiles());
+        List<String> realPortChecks = Arrays.asList("real1", "real2");
+        String defaultPortCheck = portCheck.isEmpty() ? "default" : portCheck.get(0);
+
+        return portCheck.stream()
+                .filter(realPortChecks::contains)
+                .findAny()
+                .orElse(defaultPortCheck);
+    }
+
+
+    @GetMapping("/hello")
     public String sayHello() {
         return "자동 배포 진짜 성공...!!!!";
     }
+
 }
